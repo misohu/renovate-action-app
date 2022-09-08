890c190f89ad47b13afc6eae2ccfdc23215449f1
misohu <michal.hucko@canonical.com>
2022-09-08 13:07:00 +0200

    fix: renovate config

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  branchPrefix: 'test-renovate/',
-  dryRun: true,
-  username: 'renovate-release',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
-  platform: 'github',
-  includeForks: true,
-  packageRules: [
+  "branchPrefix": "test-renovate/",
+  "dryRun": "full",
+  "username": "renovate-release",
+  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+  "onboarding": false,
+  "platform": "github",
+  "includeForks": true,
+  "packageRules": [
     {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
+      "description": "lockFileMaintenance",
+      "matchUpdateTypes": [
+        "pin",
+        "digest",
+        "patch",
+        "minor",
+        "major",
+        "lockFileMaintenance"
       ],
-      dependencyDashboardApproval: false,
-      stabilityDays: 0,
-    },
-  ],
+      "dependencyDashboardApproval": false,
+      "stabilityDays": 0
+    }
+  ]
 };
