f21cff592aaee3cd3bded9f9bb554d6ce0cc499d
misohu <michal.hucko@canonical.com>
2022-09-08 13:13:16 +0200

    fix: renovate config

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -6,19 +6,7 @@ module.exports = {
   "onboarding": false,
   "platform": "github",
   "includeForks": true,
-  "packageRules": [
-    {
-      "description": "lockFileMaintenance",
-      "matchUpdateTypes": [
-        "pin",
-        "digest",
-        "patch",
-        "minor",
-        "major",
-        "lockFileMaintenance"
-      ],
-      "dependencyDashboardApproval": false,
-      "stabilityDays": 0
-    }
-  ]
+  "pip_requirements": {
+    "fileMatch": ["requirements.txt"]
+  },
 };
