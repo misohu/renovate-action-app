d9ff68394ac2327afaff80ccf1f06401ebb55755
misohu <michal.hucko@canonical.com>
2022-09-08 13:38:29 +0200

    fix: renovate config

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -3,7 +3,7 @@ module.exports = {
   "dryRun": "full",
   "username": "renovate-release",
   "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
-  "onboarding": false,
+  "onboarding": true,
   "platform": "github",
   "includeForks": true,
   "repositories": ["misohu/renovate-action-app"]
